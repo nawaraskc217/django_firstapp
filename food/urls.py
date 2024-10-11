0c8ed5d758ceded8229dceb081f6659369a9cfc0
Nawaras kc <nawaraskc888@gmail.com>
2024-10-11 21:53:37 +0545

    fixed url

--- food/urls.py
+++ food/urls.py
@@ -4,7 +4,7 @@ from django.urls import path,include
 
 urlpatterns = [
 
-        path('',views.index,name='index'),##connects to views.py and index() function
+        path('helloo/',views.index,name='index'),##connects to views.py and index() function ,helloo/: so url is :http://127.0.0.1:8000/food/helloo/
          path('',views.show,name='show') ##connects to views.py and show() function
          
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,6 @@ from django.urls import path,include
 ##main URL
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('food/', include('food.urls')), ## food folder->urls, 'food/'-> is url
+     path('food/', include('food.urls')), ## food folder->urls, 'food/'-> is url : http://127.0.0.1:8000/food/helloo/ :: /helloo is in urls.py
       path('show/', include('food.urls')),
 ]
